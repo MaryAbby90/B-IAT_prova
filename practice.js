eac42d748fec52c89ce40c7c773f69cfdb3d8389
MaryAbby90 <mariangela_abbate@outlook.it>
2025-03-06 04:03:44 +0100

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/biat6.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
+define(['pipAPI' ,'./extension_biat.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
     "categories": [
         {
             "name": "Fiori",
